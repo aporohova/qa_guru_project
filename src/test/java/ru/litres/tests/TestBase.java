a2a64d9747b1f51571c8816a3f615c6c1a6c6c20
alena.porohova <alena.porohova@lamoda.ru>
2023-06-02 22:03:56 +0300

    test base: change annotation

--- src/test/java/ru/litres/tests/TestBase.java
+++ src/test/java/ru/litres/tests/TestBase.java
@@ -3,6 +3,7 @@ package ru.litres.tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
@@ -17,10 +18,6 @@ import java.util.Map;
 public class TestBase {
     @BeforeAll
     static void beforeAll () {
-//        Configuration.browser = System.getProperty("browser", "chrome");
-//        Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
-//        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
-//        Configuration.remote = "https://user1:1234@" + System.getProperty("remote", "selenoid.autotests.cloud/wd/hub");
         String selenoidUrl = System.getProperty("selenoid_url");
         String selenoidLoginPassword = System.getProperty("selenoid_login_password"); //format is {login}:{password}
         selenoidUrl = selenoidUrl.replaceAll("https://", "");
@@ -49,7 +46,7 @@ public class TestBase {
     }
 
 
-    @AfterEach
+    @AfterAll
     void addAttachments(){
         Attach.screenshotAs("Last screenshot");
         Attach.pageSource();
